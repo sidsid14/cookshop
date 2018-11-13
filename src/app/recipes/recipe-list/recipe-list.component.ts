b6974ab2883f0fc2c631d148ecc3347936ad2fea
sudhanshu <sudhanshu@viosmedical.com>
2018-11-13 23:17:05 +0530

    Added recipe detail template and recipe list template with logic

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipe: Recipe[] = [
+    new Recipe('A test Recipe', 'Description','https://recipes.timesofindia.com/photo/53110729.cms'),
+    new Recipe('A test 2 Recipe', 'Description 2','https://recipes.timesofindia.com/photo/53110729.cms')
+  ];
 
   constructor() { }
 
